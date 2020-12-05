8e944885f8d7026b843433fc458def26b4cd7358
spring-ryanc <spring.ryanc@gmail.com>
2020-12-05 17:10:15 -0500

    Fix work combos by using characters instead of strings

--- layers.ino
+++ layers.ino
@@ -60,17 +60,45 @@ void layer0(int key) {
 
 void layer1(int key) {
   switch (key) {
+    case 3:
+      Keyboard.press(KEY_LEFT_ALT);
+      displayCurrentKey(1, "Rename 1");
+      break;
+    case 4:
+      Keyboard.press(KEY_LEFT_ALT);
+      Keyboard.press('r');
+      displayCurrentKey(1, "Rename 2");
+      break;
+    case 5:
+      Keyboard.press(KEY_LEFT_ALT);
+      Keyboard.press('/');
+      displayCurrentKey(1, "Rename 3");
+      break;
     case 6:
       Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.press("/");
-      Keyboard.press("r");
-      displayCurrentKey(1, "Rename");
+      Keyboard.press('/');
+      Keyboard.press('r');
+      displayCurrentKey(1, "Rename Full");
+      break;
+    case 10:
+      Keyboard.press(KEY_LEFT_CTRL);
+      displayCurrentKey(1, "Goto Line 1");
+      break;
+    case 11:
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.press('g');
+      displayCurrentKey(1, "Goto Line 2");
+      break;
+    case 12:
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.press('/');
+      displayCurrentKey(1, "Goto Line 3");
       break;
     case 13:
       Keyboard.press(KEY_LEFT_CTRL);
-      Keyboard.press("/");
-      Keyboard.press("g");
-      displayCurrentKey(1, "Goto Line");
+      Keyboard.press('/');
+      Keyboard.press('g');
+      displayCurrentKey(1, "Goto Line Full");
       break;
     default:
       displayCurrentKey(1, key + ONE_OFFSET);
